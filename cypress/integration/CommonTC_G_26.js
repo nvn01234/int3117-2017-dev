0cbe22354d972b6116d8dbbdef73696a2ce42845
nvn01234 <nguyenvannhat152@gmail.com>
2017-09-23 17:54:09 +0700

    fix CommonTC_G_25, add CommonTC_G_26

--- cypress/integration/CommonTC_G_26.js
+++ cypress/integration/CommonTC_G_26.js
@@ -1,4 +1,4 @@
-it('Kiểm tra khi nhập dữ liệu là các thẻ html', function() {
+it('Kiểm tra chức năng Trim space', function() {
     // login
     cy.visit('http://52.187.8.102/');
     cy.get('body > div > div.login.ng-scope > div.content > form > div:nth-child(2) > div > input').type('admin_10@gmail.com');
@@ -10,7 +10,7 @@ it('Kiểm tra khi nhập dữ liệu là các thẻ html', function() {
 
     // add
     cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-title > div.inputs > a').click();
-    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(1) > div:nth-child(2) > input').type('</table>');
+    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(1) > div:nth-child(2) > input').type('               name                    ');
     cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(1) > div:nth-child(4) > input').type('HN');
     cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(2) > div:nth-child(2) > div.ui-select-container.select2.select2-container.ng-pristine.ng-untouched.ng-scope.ng-empty.ng-invalid.ng-invalid-required > a > span.select2-chosen.ng-binding').click();
     cy.get('#ui-select-choices-row-0-0 > div > span').click();
@@ -21,5 +21,5 @@ it('Kiểm tra khi nhập dữ liệu là các thẻ html', function() {
     cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-footer > button').click();
 
     // test
-    cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-body > div.max-width-100-pc.scrollable > table > tbody > tr:nth-child(1) > td:nth-child(3)').should('contain', '</table>');
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-body > div.max-width-100-pc.scrollable > table > tbody > tr:nth-child(1) > td:nth-child(3)').should('not.contain', ' ');
 });
