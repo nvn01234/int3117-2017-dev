5fd68415d67da83342883470b64491fe831bfa0a
nvn01234 <nguyenvannhat152@gmail.com>
2017-09-23 17:20:43 +0700

    fix CommonTC_G_24, add CommonTC_G_25

--- cypress/integration/CommonTC_G_24.js
+++ cypress/integration/CommonTC_G_24.js
@@ -1,11 +1,14 @@
 it('Tìm kiếm không phân biệt chữ hoa, chữ thường', function() {
+    // login
     cy.visit('http://52.187.8.102/');
-    cy.get('[name="email"]').type('admin_10@gmail.com');
-    cy.get('[name="password"]').type('Methadone@2017');
-    cy.get('[type="submit"]').click();
-    cy.wait(1000);
-    cy.visit('http://52.187.8.102/main/admin/administrators/issuing_agency');
-    cy.get('[ng-model="keyword"]').type('Tâm {enter}');
-    cy.wait(500);
-    cy.get('tbody > tr:nth-child(1) > td:nth-child(3)').should('contain', 'tâm');
+    cy.get('body > div > div.login.ng-scope > div.content > form > div:nth-child(2) > div > input').type('admin_10@gmail.com');
+    cy.get('body > div > div.login.ng-scope > div.content > form > div:nth-child(3) > div > input').type('Methadone@2017');
+    cy.get('body > div > div.login.ng-scope > div.content > form > button').click();
+
+    // danh sách cơ sở
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-sidebar.navbar-collapse.collapse > ul > li:nth-child(4) > a').click();
+
+    // search
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-title > div.inputs > div > div > form > input').type('Tâm {enter}');
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-body > div.max-width-100-pc.scrollable > table > tbody > tr:nth-child(1) > td:nth-child(3)').should('contain', 'tâm');
 });
---
+++ cypress/integration/CommonTC_G_25.js
@@ -0,0 +1,29 @@
+it('Tìm kiếm không phân biệt chữ hoa, chữ thường', function() {
+    // login
+    cy.visit('http://52.187.8.102/');
+    cy.get('body > div > div.login.ng-scope > div.content > form > div:nth-child(2) > div > input').type('admin_10@gmail.com');
+    cy.get('body > div > div.login.ng-scope > div.content > form > div:nth-child(3) > div > input').type('Methadone@2017');
+    cy.get('body > div > div.login.ng-scope > div.content > form > button').click();
+
+    // danh sách cơ sở
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-sidebar.navbar-collapse.collapse > ul > li:nth-child(4) > a').click();
+
+    // add
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-title > div.inputs > a').click();
+    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(1) > div:nth-child(2) > input').type('</table>');
+    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(1) > div:nth-child(4) > input').type('HN');
+    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(2) > div:nth-child(2) > div.ui-select-container.select2.select2-container.ng-pristine.ng-untouched.ng-scope.ng-empty.ng-invalid.ng-invalid-required > a > span.select2-chosen.ng-binding').click();
+    cy.get('#ui-select-choices-row-0-0 > div > span').click();
+    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(2) > div:nth-child(4) > div.ui-select-container.select2.select2-container.ng-pristine.ng-untouched.ng-scope.ng-empty.ng-invalid.ng-invalid-required > a > span.select2-chosen.ng-binding').click();
+    cy.get('#ui-select-choices-row-1-0 > div').click();
+    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-body > div > div:nth-child(2) > div:nth-child(6) > div.ui-select-container.select2.select2-container.ng-pristine.ng-untouched.ng-scope.ng-empty.ng-invalid.ng-invalid-required > a > span.select2-chosen.ng-binding').click();
+    cy.get('#ui-select-choices-row-2-0 > div').click();
+    cy.get('body > div.modal.fade.ng-scope.ng-isolate-scope.in > div > div > form > div.modal-footer > button').click();
+
+    // test
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-body > div.max-width-100-pc.scrollable > table > tbody > tr:nth-child(1) > td:nth-child(3)').should('contain', '</table>');
+
+    // delete
+    cy.get('body > div > div.page-container.ng-scope > div > div.page-content-wrapper > div > div > div > div.portlet-body > div.max-width-100-pc.scrollable > table > tbody > tr:nth-child(1) > td:nth-child(10) > a').click();
+    cy.get('body > div.bootbox.modal.fade.bootbox-confirm.in > div > div > div.modal-footer > button.btn.btn-primary').click();
+});
