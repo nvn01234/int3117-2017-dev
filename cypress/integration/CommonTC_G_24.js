a9d2fdf28417ab56700884bf597f6121dca4c982
nvn01234 <nguyenvannhat152@gmail.com>
2017-09-25 01:28:58 +0700

    fix common

--- cypress/integration/CommonTC_G_24.js
+++ cypress/integration/CommonTC_G_24.js
@@ -51,20 +51,20 @@ describe('Tìm kiếm không phân biệt chữ hoa, chữ thường', function(
         cy.get('[name="email"]').type(Cypress.env("accounts.admin.email"));
         cy.get('[name="password"]').type(Cypress.env("accounts.admin.password"));
         cy.get('[type="submit"]').click();
-        cy.wait(1000);
+        cy.wait(Cypress.env("delays.after_signin"));
     });
 
     routes.forEach(function(route) {
         it(route.name, function() {
             cy.visit(Cypress.env("routes.main." + route.name));
-            cy.wait(500);
+            cy.wait(Cypress.env("delays.after_visit"));
 
             // search
             var selector = 'tbody > tr:first-child > td:nth-child(' + route.column + ')';
             cy.get('table').eq(0).find(selector).then(function($td) {
                 var text = $td.text().trim();
-                cy.get('[placeholder="Tìm kiếm"]').type(text.toUpperCase()).type('{enter}');
-                cy.wait(500);
+                cy.get('[ng-model="keyword"]').type(text.toUpperCase()).type('{enter}');
+                cy.wait(Cypress.env("delays.after_search"));
                 cy.get('table').eq(0).find(selector).should('contain', text);
             });
         });
